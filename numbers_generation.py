6a0f63b107f77cf5509be2ef9bfcd1c4f31ff24c
Dominika <pottterd@gmail.com>
2022-08-05 14:20:24 +0300

    fix def letters

--- numbers_generation.py
+++ numbers_generation.py
@@ -3,23 +3,37 @@ import time
 time_stamp = time.time()
 
 def number_generation():
-    number = int((time_stamp % 1000 - int(time_stamp % 1000)) * 10000000000)
+    number = int((time_stamp % 1000 - int(time_stamp % 1000)) * 100000000)
     if number == 0:
         number += 1000
 
     res = [int(x) for x in str(number)]
-
+    print(res)
     for i in range(0, len(res), 2):
         res[i], res[i+1] = res[i+1], res[i]
 
     return int(''.join(map(str, res)))
 
-def add_letters():
-    # TODO запилить список латинских букв https://pythobyte.com/python-alphabet-76863/?ysclid=l6djge6cm3236271623
-    # TODO сделать генерацию списка из 5и букв
-    # TODO Выводить одной строкой
-    pass
-    return 42
-
 print(number_generation())
-print(add_letters())
+
+alphabets_in_lowercase = []
+def add_letters():
+    for i in range(97, 107):
+        alphabets_in_lowercase.append(chr(i))
+    # print(alphabets_in_lowercase)
+    number = int(time_stamp % 100000)
+    if number == 0:
+        number += 100000
+    number_list = [int(x) for x in str(number)]
+    result = list()
+    for i in range(len(number_list)):
+        result.append(alphabets_in_lowercase[number_list[i]])
+    return result
+
+print(''.join(map(str, add_letters())))
+
+
+
+
+
+
